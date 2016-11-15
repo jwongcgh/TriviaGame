2bdc7977dd8f26ff09930b0a3836d2ed82dcf017
Jorge Wong <JW@Jorges-MBP.attlocal.net>
2016-11-15 11:48:34 -0600

    initial setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+console.log("link working");
+
+// ********************************* variables and library here ********************************* //
+
+
+
+// ************************************* count down here ************************************* //
+// listen to start game button 
+// countdown 1 shows to allow readyness of player
+// calls function to show up randomly selected question and pertinent choices
+// a countdown for player to pick answer runs when question/choices are displayed
+
+// ************************************* Pick question ************************************* //
+// randomly select question and relevant choices
+// first: clear pre-game count down
+// second: pick question
+// third: display question
+// fourth: display main countdown, go to compare to answer
+// remove question and choices pair from library
+// keep track of number of times this was accessed
+// keep track of length of library, call on end game function
+// call on click event here
+
+
+// ************************************* on click event here *********************************** //
+// listen to click 
+// allow for change of answer while time is not over
+// add button to submit
+// compare to answer function
+
+
+// ************************************* compare to answer ************************************* //
+// was choice the correct one?
+// if yes, show congrats! Pause here for a short pause before next question.
+// if empty or wrong choice, show wrong!, show corrrect answer, a short pause before next question
+// keep track of wins and losses
+// retreive new question
+
+// ************************************* end game ************************************* //
+// show stats: wins, losses
+// show "start new game" button
+
+
+// ************************************* start new game ************************************* //
+// button for start new game if player quits current game or game ends
+
+
+}); // end document ready
